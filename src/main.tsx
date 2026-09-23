9a8332b30f9aad9c82fee11e259b7615ae681a71
devarshshiroya5e-hub <devarshshiroya5e@gmail.com>
2026-09-23 20:57:33 +0530

    Load MornAI motion polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { Component, ErrorInfo, ReactNode, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './mornaiMotionPolish.css';
 import { installMornaiNavigationPolish } from './mornaiNavigationPolish';
 
 installMornaiNavigationPolish();
