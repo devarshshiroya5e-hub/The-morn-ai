2b3013dcd943fa17fb80332506434f076444c000
devarshshiroya5e-hub <devarshshiroya5e@gmail.com>
2026-09-23 20:57:14 +0530

    Enable smooth page transition scroll reset

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { Component, ErrorInfo, ReactNode, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { installMornaiNavigationPolish } from './mornaiNavigationPolish';
+
+installMornaiNavigationPolish();
 
 class RuntimeErrorBoundary extends Component<{ children: ReactNode }, { error: Error | null }> {
   private readonly children: ReactNode;
